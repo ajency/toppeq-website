4c67a1b4853d0c5b77eae967ac472eb5e6b295da
Ankita <ankita@ajency.in>
2020-01-13 17:30:42 +0530

    services & why toppq section

--- themes/toppeq/static/js/app.js
+++ themes/toppeq/static/js/app.js
@@ -5,5 +5,17 @@ $(document).ready(function () {
         arrows: false,
         dots: true        
     });
-
+    
+	$('.pageSlider').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow:1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: true,
+		prevArrow:'<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+		nextArrow:'<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+	});
 });
