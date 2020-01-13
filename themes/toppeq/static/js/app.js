38e9ea099e09a9570c346d838fd6bc8b722827f6
Vaibhav Arolkar <vaibhav.a@ajency.in>
2020-01-13 16:24:28 +0530

    Header menu

--- themes/toppeq/static/js/app.js
+++ themes/toppeq/static/js/app.js
@@ -6,4 +6,16 @@ $(document).ready(function () {
         dots: true        
     });
 
+    $(".hamburger-menu").click(function(e) {
+		e.preventDefault();
+        $(".site-menu").addClass("visible");
+        $("body").addClass("overflow-hidden");
+    });
+    
+    $(".menu-close-btn").click(function(e) {
+		e.preventDefault();
+        $(".site-menu").removeClass("visible");
+        $("body").removeClass("overflow-hidden");
+	});
+
 });
