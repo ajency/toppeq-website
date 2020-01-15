f4d712831bf785d7f0eb605269ca100338795001
Ankita <ankita@ajency.in>
2020-01-15 09:21:57 +0530

    added animation

--- themes/toppeq/static/js/app.js
+++ themes/toppeq/static/js/app.js
@@ -32,4 +32,34 @@ $(document).ready(function () {
 		    }
 		  ]
 	});
+	var animateHTML = function() {
+	var elems,
+	windowHeight
+
+	var init = function() {
+		elems = document.getElementsByClassName("hidden");
+		windowHeight = window.innerHeight;
+		_addEventHandlers();
+	}
+
+	var _addEventHandlers = function() {
+		window.addEventListener("scroll", _checkPosition);
+		window.addEventListener("resize", init)
+	}
+	var _checkPosition = function() {
+		for ( var i = 0; i < elems.length; i++ ) {
+		var posFromTop = elems[i].getBoundingClientRect().top;
+		if ( posFromTop - windowHeight <= 0) { 
+			elems[i].className = elems[i].className.replace( "hidden", "in-left" );
+		}
+	}    
+}
+
+  return {
+    init: init
+  }
+}
+
+animateHTML().init();
+
 });
